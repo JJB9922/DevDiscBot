84169c6ee58e4b6d3ac2f34673ac9f321d3c4a3e
Jj <JJB9922@protonmail.com>
2024-02-14 21:41:22 +0000

    added credits command

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 import discord
 import dotenv
 import os
+#from humblescraper import check_humble_bundle
+import DevLinkCredits
 
 dotenv.load_dotenv()
 botToken = os.getenv('BOT_API_TOKEN')
@@ -21,5 +23,17 @@ async def on_message(message):
 
     if message.content.startswith('d.ping'):
         await message.channel.send(f'Pong! {round(client.latency * 1000)}ms')
+        
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('d.makers'):
+        await message.channel.send("My makers are:\n")
+        for member in DevLinkCredits.devList:
+            await message.channel.send(f'<@{DevLinkCredits.devList[member]}>\n')
+        
+#check_humble_bundle(client)
 
 client.run(botToken)
