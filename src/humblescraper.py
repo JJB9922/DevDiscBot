43904c7240185a057d2683c22ca9fd227e1be33c
Jj <JJB9922@protonmail.com>
2024-02-14 23:37:03 +0000

    humble bundle scraper that runs every 24h

---
+++ src/humblescraper.py
@@ -0,0 +1,102 @@
+import json
+import requests
+from bs4 import BeautifulSoup
+import discord
+import os
+
+keywords = [
+    'java', 
+    'python',
+    'C++',
+    'C#',
+    'web development',
+    'html',
+    'css',
+    'full stack',
+    'ux',
+    'javascript', 
+    'ruby',
+    'cybersecurity',
+    'security',
+    'hacker',
+    'hacking',
+    'cloud',
+    'game development'
+    'game design',
+    'programming',
+    'unity',
+    'unreal',
+    'linux'
+]
+
+def extract_json_data(url):
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    
+    script_tag = soup.find('script', id='landingPage-json-data')
+    json_data = json.loads(script_tag.contents[0].strip())
+    
+    return json_data
+
+def filter_and_save_books_with_keywords_to_file(json_data, filename):
+    data = json_data.get('data', {})
+    books_data = data.get('books', [])
+    mosaic_data = books_data.get('mosaic', [])
+    bundles_data = mosaic_data[0].get('products', [])
+    
+    filtered_books = []
+
+    for product in bundles_data:
+        tile_name = product.get('tile_name', '').lower()
+        short_marketing_blurb = product.get('short_marketing_blurb', '')
+        product_url = product.get('product_url', '')
+        image_url = product.get('tile_image', '')
+
+        if any(keyword in tile_name for keyword in keywords):
+            filtered_books.append({
+                'title': tile_name,
+                'description': short_marketing_blurb,
+                'url': product_url,
+                'image_url': image_url
+            })
+
+    with open(filename, 'w') as file:
+        json.dump(filtered_books, file, indent=4, sort_keys=True)
+
+def load_books_from_file(filename):
+    with open(filename, 'r') as file:
+        return json.load(file)
+
+def run_humble_check():
+    url = 'https://www.humblebundle.com/books'
+    json_data = extract_json_data(url)
+    filename = 'data/filtered_books.json' 
+    filter_and_save_books_with_keywords_to_file(json_data, filename)
+    print('Latest humble bundle data saved to file')
+
+async def send_humble_embed(embed_data, client):
+    embed = discord.Embed(
+        title=embed_data['title'].title(),
+        description=embed_data['description'],
+        color=0xBDFFBD
+    )
+    embed.add_field(name="Click Here to Buy", value=f"https://www.humblebundle.com{embed_data['url']}", inline=False)
+    embed.set_image(url=embed_data['image_url'])
+    await client.get_channel(1207442996161814578).send(embed=embed)
+
+async def check_and_send_new_bundles(client):
+    filename = 'data/filtered_books.json'
+    
+    if not os.path.exists(filename):
+        with open(filename, 'w') as file:
+            file.write('[]')
+        
+    current_books = load_books_from_file(filename)
+    run_humble_check()
+    new_books = load_books_from_file(filename)
+    
+    difference_books = [book for book in new_books if book not in current_books]
+
+    for bundle in difference_books:
+        await send_humble_embed(bundle, client)
+
--- src/main.py
+++ src/main.py
@@ -1,8 +1,13 @@
 import discord
 import dotenv
 import os
-#from humblescraper import check_humble_bundle
 import DevLinkCredits
+import requests
+from bs4 import BeautifulSoup
+import discord
+import json
+from humblescraper import run_humble_check, check_and_send_new_bundles
+import asyncio
 
 dotenv.load_dotenv()
 botToken = os.getenv('BOT_API_TOKEN')
@@ -15,6 +20,8 @@ client = discord.Client(intents=intents)
 @client.event
 async def on_ready():
     print(f'Logged in as {client.user}')
+    print("Starting daily bundle checker...")
+    await check_daily_bundles(client);
 
 @client.event
 async def on_message(message):
@@ -33,7 +40,18 @@ async def on_message(message):
         await message.channel.send("My makers are:\n")
         for member in DevLinkCredits.devList:
             await message.channel.send(f'<@{DevLinkCredits.devList[member]}>\n')
-        
-#check_humble_bundle(client)
 
+@client.event
+async def on_message(message):
+    if message.author == client.user: 
+        return
+
+    if message.content.startswith('d.humble') and message.author.id in DevLinkCredits.devList.values():
+        await check_and_send_new_bundles(client)  
+        
+async def check_daily_bundles(client):
+    while True:
+        await check_and_send_new_bundles(client)
+        await asyncio.sleep(24 * 3600) 
+        
 client.run(botToken)
